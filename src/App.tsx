5ebec73c5c051114ac0ce1a6a7d955fc0c09144a
philipp.weikert <philippweikert3@gmail.com>
2022-03-03 09:15:47 +0100

    stillugly

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import { Link, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <div>
+        <h1>Rick und Morty Charaktere</h1>
+      </div>
       <div><Link to="gallery">Galerie</Link> </div>
         <div><Outlet /></div>
         <div>Footer</div>
+      <div>
+        <p>Für Fragen und/oder Anregungen wendet euch gerne an ichantworte@nichtaufmails.de</p>
+      </div>
     </div>
   );
 }
